6f178457256305b7bcc4dec0b0d50d156f5e3afc
Cole <cole+codebase@ournameismud.co.uk>
2019-09-19 15:27:45 +0100

    version update

--- src/AcousticApp.php
+++ src/AcousticApp.php
@@ -58,7 +58,7 @@ class AcousticApp extends Plugin
     /**
      * @var string
      */
-    public $schemaVersion = '1.0.14';
+    public $schemaVersion = '1.0.15';
     public $cols = array(
         'Date of test' => 'test_testDate',
         'Acoustic Rating' => 'test_dB',
